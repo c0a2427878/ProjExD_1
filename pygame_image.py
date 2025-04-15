c6cbc373dba4ef92c2f4547e25f1d2b68879909d
Watanabe Issei <c0a2427878@edu.teu.ac.jp>
2025-04-15 16:45:32 +0900

    ループ

--- pygame_image.py
+++ pygame_image.py
@@ -11,20 +11,22 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.image.load("fig/pg_bg.jpg")
-    bg_img2 = pg.transform.flip(bg_img2, True, False) #練習
+    bg_img2 = pg.transform.flip(bg_img2, True, False) #練習8
     kk_img = pg.image.load("fig/3.png") #練習2
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
         x = tmr
+        if tmr == 3200: tmr = 0 #練習9
         screen.blit(bg_img, [-x, 0]) #練習6
         screen.blit(bg_img2,[-x+1600,0]) #練習7
+        screen.blit(bg_img,[-x+3200,0])
         screen.blit(kk_img,[300,200]) #練習4
         pg.display.update()
-        tmr += 1        
+        tmr += 1
+                
         clock.tick(200) #練習5
 
 
