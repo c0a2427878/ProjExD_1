151404936071194ac54b73c74d3008d4bb6066a0
Watanabe Issei <c0a2427878@edu.teu.ac.jp>
2025-04-15 15:55:35 +0900

    横300 縦200

--- pygame_image.py
+++ pygame_image.py
@@ -15,8 +15,8 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
